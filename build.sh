540765a91951f0eada49d9628acaec3cb1ec2742
DarkAngelGR <asavvo01@gmail.com>
2019-03-20 23:40:58 +0000

    Add compiling script

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+export ARCH=arm64 && export SUBARCH=arm64
+export CROSS_COMPILE=~/Toolchain/bin/aarch64-linux-gnu-
+export USE_CCACHE=1
+export CCACHE_DIR=~/.ccache
+make O=out clean
+make O=out mrproper
+make O=out opympian_markw_defconfig
+make O=out -j8
+mv out/arch/arm64/boot/Image.gz-dtb zip/
+cd zip
+zip -r9 olympiankernel_zeus-$(date +"%Y-%m-%d").zip *
+gdrive upload olympiankernel_zeus*.zip
+rm -rf Image.gz-dtb *.zip
+
